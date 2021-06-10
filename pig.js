8858c6a43c1af0a968f397fbd74edc4c8a9835f7
hansika2 <68047113+hansika2@users.noreply.github.com>
2021-06-10 15:41:51 +0530

    Add files via upload

---
+++ pig.js
@@ -0,0 +1,26 @@
+class Pig {
+    constructor(x, y) {
+      var options = {
+          'restitution':0.8,
+          'friction':0.9,
+          'density':1.0
+      }
+      this.body = Bodies.rectangle(x, y,45,40, options);
+      this.width = 45;
+      this.height = 40;
+      
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      var angle = this.body.angle;
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      rectMode(CENTER);
+      fill("green");
+      rect(0, 0, this.width, this.height);
+      pop();
+    }
+  };
+  
